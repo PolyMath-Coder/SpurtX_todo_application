b75b042bec68926f46105f5a515cdf90f54afcce
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-04-29 15:55:15 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+
+@Module({
+  imports: [AuthModule, UserModule, TodoModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
